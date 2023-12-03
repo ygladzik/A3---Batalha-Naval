bacd182d11e9d789d7da788d38c4cf56844cfec1
GabrielRebes <149078383+GabrielRebes@users.noreply.github.com>
2023-12-03 18:11:41 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,31 +9,31 @@ app = FastAPI()
 
 @app.get("/")
 async def lista_jogadores():
-    return JogadorController.get_instance().lista_todos_os_jogadores()
+    return {"massage": "JogadorController.get_instance().lista_todos_os_jogadores()"}
 
 @app.get("/top3")
 async def ranking_top3():
-    return JogadorController.get_instance().lista_ranking_top_3()
-
+    return {"message":"JogadorController.get_instance().lista_ranking_top_3()"}
+    
 @app.get("/criar_partida")
 async def criar_partida():
     id = str(Partida.get_novo_id())
     response = RedirectResponse(url='/entrar_partida/'+id)
-    return response
+    return {"message":"response"}
 
 @app.get("/entrar_partida/{id}")
 async def get_tabuleiro():
     #byte = bytes(TabuleiroParte.get_tabuleiro(), 'utf-8')
-    return TabuleiroParte.get_tabuleiro()
+    return {"message":"TabuleiroParte.get_tabuleiro()"}
 
 @app.patch("/entrar_partida/{id}/colocar/{x}/{y}/{barco}/{rot}")
 async def set_tabuleiro_volta(x: int, y: int, emb: str, rot: int):
     TabuleiroParte.set_tabuleiro(emb, x, y, rot)
     response = RedirectResponse(url='/entrar_partida/{id}')
-    return response
+    return {"message":"response"}
 
 @app.get("/entrar_partida/{id}/atacar/{x}/{y}")
 async def set_tabuleiro_atira_volta(x: int, y: int):
     TabuleiroParte.jogada(x, y)
     response = RedirectResponse(url='/entrar_partida/{id}')
-    return response
+    return {"message":"response"}
