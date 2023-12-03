1c8cd7acbe1a87ddfdf8426b1a14afc77a0f214d
Rasiver <gabrielmmarques28@gmail.com>
2023-12-03 14:12:25 -0300

    testando o banco

--- batalha_naval/data/jogador_db.py
+++ batalha_naval/data/jogador_db.py
@@ -9,37 +9,35 @@ class JogadorDB():
 
     _lista_de_jogadores = []
 
-    # def popula_do_banco(self):
-    #     # lê do banco de dados
-    #     with sqlite3.connect("batalha_banco.sqlite") as conn:
-
-    #         cursor = conn.cursor()
-    #         res = cursor.execute("SELECT id, apelido, email, senha, pontuacao FROM Jogadores")
-
-    #         for r in res:
-    #             j = Jogador(
-    #                 apelido=r[1],
-    #                 email=r[2],
-    #                 senha=r[3]
-    #             )
-    #             j._pontuacao_acumulada = r[4]
-    #             self._lista_de_jogadores.append(j)
-
-    def popula_do_arquivo(self):
-        with open(self.FONTE_DOS_DADOS) as f:
-                linhas = f.readlines()
-                for linha in linhas:
-                    # trim() -> remove espaços no início e final da string
-                    # no python, trim se chama "strip"
-                    if linha.strip() != "":
-                        print(linha)
-                        v = linha.replace("\n","").split(";")
-                        j = Jogador(v[0], v[2], v[3])
-                        j._pontuacao_acumulada = v[1]
-                        self._lista_de_jogadores.append(j)
-
-    def __init__(self):
-            self.popula_do_arquivo()
-
-    def lista_todos_os_jogadores(self):
+def popula_do_banco(self):
+         # lê do banco de dados
+         with sqlite3.connect("batalha_banco.sqlite") as conn:
+             cursor = conn.cursor()
+             res = cursor.execute("SELECT id, apelido, email, senha, pontuacao FROM Jogadores")
+             for r in res:
+                 j = Jogador(
+                     apelido=r[1],
+                     email=r[2],
+                     senha=r[3]
+                 )
+                 j._pontuacao_acumulada = r[4]
+                 self._lista_de_jogadores.append(j)
+#
+    #def popula_do_arquivo(self):
+    #    with open(self.FONTE_DOS_DADOS) as f:
+    #            linhas = f.readlines()
+    #            for linha in linhas:
+    #                # trim() -> remove espaços no início e final da string
+    #                # no python, trim se chama "strip"
+    #                if linha.strip() != "":
+    #                    print(linha)
+    #                    v = linha.replace("\n","").split(";")
+    #                    j = Jogador(v[0], v[2], v[3])
+    #                    j._pontuacao_acumulada = v[1]
+    #                    self._lista_de_jogadores.append(j)
+#
+def __init__(self):
+           self.popula_do_banco()
+#
+def lista_todos_os_jogadores(self):
         return self._lista_de_jogadores
